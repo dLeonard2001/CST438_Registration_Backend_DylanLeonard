8d796b5fe9a104107b5c047e1758b4718a3a85e9
Dylan Leonard <dleonard@csumb.edu>
2022-10-25 20:52:06 -0700

    final commit

--- cst438_register/src/main/java/com/cst438/controller/CourseController.java
+++ cst438_register/src/main/java/com/cst438/controller/CourseController.java
@@ -1,6 +1,21 @@
 package com.cst438.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cst438.domain.Course;
+import com.cst438.domain.CourseDTOG;
+import com.cst438.domain.CourseRepository;
+import com.cst438.domain.Enrollment;
+import com.cst438.domain.EnrollmentRepository;
+import com.cst438.domain.Student;
+import com.cst438.domain.StudentRepository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,6 +32,12 @@ public class CourseController {
 	@Autowired
 	EnrollmentRepository enrollmentRepository;
 	
+	@Autowired
+	CourseRepository courseRepository;
+	
+	@Autowired
+	StudentRepository studentRepository;
+	
 	/*
 	 * endpoint used by gradebook service to transfer final course grades
 	 */
@@ -24,8 +45,11 @@ public class CourseController {
 	@Transactional
 	public void updateCourseGrades( @RequestBody CourseDTOG courseDTO, @PathVariable("course_id") int course_id) {
 		
-		//TODO  complete this method in homework 4
-		
+		for (CourseDTOG.GradeDTO g : courseDTO.grades) {
+			Enrollment e = enrollmentRepository.findByEmailAndCourseId(g.student_email, course_id);
+			e.setCourseGrade(g.grade);
+			enrollmentRepository.save(e);
+		}
 	}
 
 }
